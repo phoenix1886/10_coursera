e091fe4d193e3a8a3c985f132d649e66f24d75d7
Kirill Maslov <maslovks@gmail.com>
2017-12-15 10:14:28 +0300

    second iteration

--- coursera.py
+++ coursera.py
@@ -14,7 +14,7 @@ def parse_arguments():
     return args.path, args.num
 
 
-def get_courses_list():
+def fetch_courses_list():
     site_map_url = 'https://www.coursera.org/sitemap~www~courses.xml'
     site_map = requests.get(site_map_url)
     xml_tree = etree.fromstring(site_map.content)
@@ -25,34 +25,28 @@ def get_courses_list():
 
 
 def fetch_html_tree(course_url):
-    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) '
-                             'AppleWebKit/537.36 (KHTML, like Gecko) '
-                             'Chrome/50.0.2661.102 Safari/537.36'}
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) '
+                      'AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/50.0.2661.102 Safari/537.36',
+    }
     request = requests.get(course_url, headers=headers)
     request.raise_for_status()
     raw_html = request.content
-    html_tree = BeautifulSoup(raw_html, 'lxml')
-    return html_tree
+    soup = BeautifulSoup(raw_html, 'lxml')
+    return soup
 
 
-def get_course_info(html_tree):
-    course_info = dict()
-    course_info['title'] = html_tree.find(
-        'h1',
-        class_='title display-3-text').get_text()
-    course_info['lang'] = html_tree.find(
-        'div',
-        class_='rc-Language').get_text()
-    score_element = html_tree.find(
-        'div',
-        class_='ratings-text bt3-visible-xs')
+def get_course_info(soup):
+    course_info = {}
+    course_info['title'] = soup.find('h1',
+                                     class_='title display-3-text').get_text()
+    course_info['lang'] = soup.find('div', class_='rc-Language').get_text()
+    score_element = soup.find('div', class_='ratings-text bt3-visible-xs')
     course_info['av_score'] = (score_element.get_text()
                                if score_element else 'No scores')
-    course_info['course_length'] = len(
-        html_tree.find_all('div', class_='week'))
-    course_info['start_dt'] = html_tree.find(
-        'div',
-        class_='startdate').get_text()
+    course_info['course_length'] = len(soup.find_all('div', class_='week'))
+    course_info['start_dt'] = soup.find('div', class_='startdate').get_text()
     return course_info
 
 
@@ -64,7 +58,8 @@ def output_courses_info_to_xlsx(file_path, courses):
         'Language',
         'User\'s score',
         'Course length',
-        'Start date']
+        'Start date',
+    ]
 
     work_sheet.append(header)
     for course in courses:
@@ -73,14 +68,14 @@ def output_courses_info_to_xlsx(file_path, courses):
             course['lang'],
             course['av_score'],
             course['course_length'],
-            course['start_dt']
+            course['start_dt'],
         ])
     work_book.save(file_path)
 
 
 if __name__ == '__main__':
     file_path, num_of_courses = parse_arguments()
-    courses_urls = get_courses_list()
+    courses_urls = fetch_courses_list()
     courses_urls = sample(courses_urls, num_of_courses)
 
     print('Requesting coursera...')
@@ -88,12 +83,12 @@ if __name__ == '__main__':
     for course_url in courses_urls:
         try:
             print('parsing {}'.format(course_url))
-            html_tree = fetch_html_tree(course_url)
-            courses_info.append(get_course_info(html_tree))
+            soup = fetch_html_tree(course_url)
+            courses_info.append(get_course_info(soup))
         except requests.exceptions.HTTPError as err:
             print('Couldn\'t request information to {}. {}'.format(
                 course_url, err))
-        except AttributeError as err:
+        except AttributeError:
             print('Couldn\'t parse web page {}'.format(course_url))
 
     output_courses_info_to_xlsx(file_path, courses_info)
